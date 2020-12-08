76565b40788011aeb9e7763ed6c10a2b52440b7b
KwstasKnd <75222829+KwstasKnd@users.noreply.github.com>
2020-12-08 15:56:10 +0200

    ver.1 [hub] (main.py & hanoi.py only)

---
+++ hanoi.py
@@ -0,0 +1,40 @@
+from util import *
+
+
+
+def hanoi(domainInstance):
+	if( isValid(domainInstance) ):
+		N = len(domainInstance[0])
+		return rec_hanoi(domainInstance,N,FIRST,THIRD) # move N disks from column 1 to column 3
+	else:
+		print("hanoi: Error,invalid domainInstance given as argument")
+		return ["ERROR"]
+
+
+
+def rec_hanoi(columns,N,From,To):
+	if(N==1):
+		top = (len(columns[From]) - 1)
+		disk = columns[From][top]
+		columns[From].remove(disk)
+		columns[To].append(disk)
+		move = str(From+1) + "-->" + str(To+1)
+		return [move]
+	
+	Other = -1
+	for c in range(0,2+1):
+			if( (From != c) and (To != c) ):
+				Other = c
+				break
+	# Goal: move N disks from columns[From] to column columns[To],using columns[Other] as an intermediate
+	
+	# Step 1: move the N-1 smaller disks from columns[From] to column columns[Other]
+	moves1 = rec_hanoi(columns,N-1,From,Other)
+
+	# Step 2: move the larger disk from columns[From] to column columns[To]
+	moves2 = rec_hanoi(columns,1,From,To)
+
+	# Step 3: move the N-1 smaller disks from columns[Other] to column columns[To]
+	moves3 = rec_hanoi(columns,N-1,Other,To)
+	
+	return moves1 + moves2 + moves3
---
+++ main.py
@@ -0,0 +1,34 @@
+from util import *
+from hanoi import *
+
+
+
+def main(maxN = 8):
+	print("")
+	for N in range(1,maxN+1):
+		domainInstance = generateHanoiInstance(N)
+		print("hanoi(" + str(N) + ")")
+		#print("Initial state: ", end="")
+		#print(domainInstance)
+
+		solution = hanoi(domainInstance)
+		#print("Solution: ", end="")
+		#print(solution, end="")
+		#print("    ", end="")
+
+		movesRequired = pow(2,N)-1
+		movesMade = len(solution)
+		if(movesRequired == movesMade):
+			print("(moves: " + str(movesMade) + ")")
+		else:
+			print("main: Error,moves made (" + str(movesMade) + ") are not equal to the moves required (" + str(movesRequired) + ")")
+
+		#print("Final state: ", end="")
+		#print(domainInstance)
+
+		if(N<maxN):
+			print("--------------------------------------------------------------------------------")
+
+
+
+main(20)
